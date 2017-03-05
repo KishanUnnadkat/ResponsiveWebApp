52605384cf4818b57f789140483d552dce208179
Kishan Unnadkat <kunnadkat@Kishans-MacBook-Pro-2.local>
2017-03-05 19:10:21 +0000

    Working username and picture on login

--- js/auth0Load.js
+++ js/auth0Load.js
@@ -3,15 +3,15 @@
     document.getElementById('btn-login').addEventListener('click', function() {
       lock.show(function(err, profile, token) {
         if (err) {
-          // Error callback
           console.error("Something went wrong: ", err);
         } else {
-          // Success calback  
-
-          // Save the JWT token.
           localStorage.setItem('userToken', token);
-          // Save the profile
           localStorage.setItem('userProfile', JSON.stringify(profile));
+          document.getElementById('nick').textContent = profile.name;
+
+          document.getElementById('userProfilePic').src = profile.picture;
         }
       });
     });
+
+
